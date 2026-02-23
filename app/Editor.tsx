9558c36c9fe7da41923202598218dc358b59296f
donggri <99480@naver.com>
2026-02-23 16:27:16 +0900

    uploadFile func add

--- app/Editor.tsx
+++ app/Editor.tsx
@@ -4,12 +4,38 @@ import { useCreateBlockNoteWithLiveblocks } from "@liveblocks/react-blocknote";
 import { BlockNoteView } from "@blocknote/mantine";
 import { Threads } from "./Threads";
 
+async function uploadFile(file: File) {
+  const body = new FormData();
+  body.append("file", file);
+  const ret = await fetch("https://tmpfiles.org/api/v1/upload", {
+    method: "POST",
+    body: body,
+  });
+  return (await ret.json()).data.url.replace(
+    "tmpfiles.org/",
+    "tmpfiles.org/dl/",
+  );
+}
+
 export function Editor() {
-  const editor = useCreateBlockNoteWithLiveblocks({});
+  const editor = useCreateBlockNoteWithLiveblocks(
+    { uploadFile },
+    {
+      offlineSupport_experimental: true,
+    },
+  );
 
   return (
     <div>
-      <BlockNoteView editor={editor} className="editor" />
+      <BlockNoteView
+        editor={editor}
+        className="editor"
+        editable={true}
+        onChange={(editor, { getChanges }) => {
+          const changes = getChanges();
+          console.log("changes:", changes);
+        }}
+      />
       <Threads editor={editor} />
     </div>
   );
